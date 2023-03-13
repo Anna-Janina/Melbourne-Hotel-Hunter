ad2a160269f69b934f4b6c712a0f81a37d856e74
zafarfast <zafar_fast@hotmail.com>
2023-03-13 19:09:36 +1100

    added signout route

--- controller/index.js
+++ controller/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 
 const signinRoute = require('./api/sign-in');
 const signupRoute = require('./api/sign-up');
+const signoutRoute = require('./api/sign-out');
+
 const homeRoute = require('./homeRoute');
 
 router.use('/', homeRoute);
 router.use('/signin', signinRoute);
 router.use('/signup', signupRoute);
+router.use('/signout', signoutRoute);
+
 module.exports = router;
