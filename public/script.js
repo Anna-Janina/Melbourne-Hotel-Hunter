08fe2867bd9a9c605835af8c03c78487d67bc5ae
zafarfast <zafar_fast@hotmail.com>
2023-03-15 01:01:12 +1100

    commented the JS file

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+// function login() will send sign-in request to the backend server
+
 function login()
 {
 
@@ -32,20 +34,24 @@ function login()
     })
 }
 
+
+// function signup() will send sign-up request to the backend server
+
 function signup()
 {
 
-    const firstname = document.getElementById('name').value;
-    const lastname = document.getElementById('surname').value;
-    const email = document.getElementById('Email').value;
-    const password = document.getElementById('SignupPassword').value;
-    const rePassword = document.getElementById('reEnterPassword').value;
+    const firstname = document.getElementById('name').value; //get user first name
+    const lastname = document.getElementById('surname').value; //get user last name
+    const email = document.getElementById('Email').value; //get user email address 
+    const password = document.getElementById('SignupPassword').value; //get password
+    const rePassword = document.getElementById('reEnterPassword').value; //get re entered password
 
-    if (password != rePassword)
+    if (password != rePassword) // check if both passwords match
     {
-        document.getElementById('error-message2').textContent = "Passwords do not match";
-        return
+        document.getElementById('error-message2').textContent = "Passwords do not match"; //display message to user
+        return // return if passwords don't match
     }
+    //send POST request to bacnend server with user details
     fetch('/signup', {
         method: 'POST',
         headers: {
@@ -67,6 +73,7 @@ function signup()
     
     .then((data)=>{
         console.log(data)
+        //display message to the user based on server response
         if (data == 'Email already exist')
         {document.getElementById('error-message2').textContent = "Email already exist";}
         else if (data == 'Fill out all fields')
@@ -84,18 +91,19 @@ function signup()
 
         }
         else
-        {
+        {   //redirect user to home page
             window.location.replace('/')
         }
     })
 }
 
+// function submitReview() will send user review to the backend server
 
 function submitReview()
 {
-    const heading = document.getElementById('review-heading').value;
-    const review = document.getElementById('code').value;
-    const hotelid = document.getElementById('submit-review').getAttribute('data-id')
+    const heading = document.getElementById('review-heading').value; //get the heading text
+    const review = document.getElementById('code').value; //get the review text
+    const hotelid = document.getElementById('submit-review').getAttribute('data-id') //get hotel id
 
     fetch(`/review/${hotelid}`,{
         method: 'POST',
@@ -112,7 +120,7 @@ function submitReview()
     
     .then((response)=>{
         if (response.status == '200')
-        {
+        {   
             document.getElementById('review-submit-message').value = "Your review has been submitted";
             window.location.reload(`/review/${hotelid}`)
         }
@@ -120,26 +128,34 @@ function submitReview()
 
 }
 
-
+//Check if the "login-button" id exist on the page. This could be missing depending on which handlebar we are on
 if (document.getElementById("login-button"))
 {
+
+// add event listner on login button
 
 document.getElementById("login-button").addEventListener("click", (event)=>{
     event.preventDefault()
     login(event)});
 }
+//Check if the "submit-review" id exist on the page. This could be missing depending on which handlebar we are on
 
 if (document.getElementById("submit-review"))
 {
 
+// add event listner on submit review button
 document.getElementById("submit-review").addEventListener("click", (event)=>{
     event.preventDefault()
     submitReview()});
 }
 
+//Check if the "submit-signup" id exist on the page. This could be missing depending on which handlebar we are on
+
 if (document.getElementById("submit-signup"))
 {
 
+// add event listner on submit signup button
+    
 document.getElementById("submit-signup").addEventListener("click", (event)=>{
     event.preventDefault()
     signup()});
